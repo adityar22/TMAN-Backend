40dc9e3527f481d68cb56becd07bcd9e0ed280e4
adityar22 <adityaramadhanarckarito47@gmail.com>
2022-12-01 23:17:31 +0700

    add allowance

--- index.js
+++ index.js
@@ -26,6 +26,16 @@ app.use(cors({
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+app.use((req, res, next) => {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();
+});
+
 app.use('/api/tasks', taskRouter);
 app.use('/api/user', userRouter);
 app.use('/api/notes', notesRouter);
